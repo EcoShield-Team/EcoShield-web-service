d551cb38194a26ed135030287e1d84d2d4c81dee
Gerardo Chavez <gerardomanuelrichard@gmail.com>
2025-09-08 01:52:01 -0500

    First Commit

---
+++ src/main/java/com/api/ecoshieldwebservice/EcoShieldWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.ecoshieldwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoShieldWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcoShieldWebServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/ecoshieldwebservice/EcoShieldWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ecoshieldwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoShieldWebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
