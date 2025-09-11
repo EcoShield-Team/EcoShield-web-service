98134775ca0936c4d74527c1631186a8d7344aec
Gerardo Chavez <gerardomanuelrichard@gmail.com>
2025-09-10 23:50:40 -0500

    asd

--- src/main/java/com/api/ecoshieldwebservice/controllers/AuthController.java
+++ src/main/java/com/api/ecoshieldwebservice/controllers/AuthController.java
@@ -7,9 +7,11 @@ import com.api.ecoshieldwebservice.dtos.UsuarioRegisterDTO;
 import com.api.ecoshieldwebservice.services.AuthService;
 import com.api.ecoshieldwebservice.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Service
 public class AuthController {
 
     @Autowired
