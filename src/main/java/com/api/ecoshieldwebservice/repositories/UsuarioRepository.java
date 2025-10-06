faa7b7eab68a1236b2bf939e9d9253f16fabc65b
Gerardo Chavez <gerardomanuelrichard@gmail.com>
2025-10-05 19:12:32 -0500

    feat: finish

--- src/main/java/com/api/ecoshieldwebservice/repositories/UsuarioRepository.java
+++ src/main/java/com/api/ecoshieldwebservice/repositories/UsuarioRepository.java
@@ -10,5 +10,4 @@ import java.util.Optional;
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
     Optional<Usuario> findByUsuarioCorreo(String usuariocorreo);
     boolean existsByUsuarioCorreo(String usuariocorreo);
-    List<Usuario> findByUsuarioEstado(UsuarioEstado usuarioEstado);
 }
